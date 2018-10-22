53babc5c12b5d58e05118d7297ec64cdb653b29f
runderwood <runderwood2016@gmail.com>
2018-10-22 15:02:51 -0500

    attempting to add title and blog to database

--- main.py
+++ main.py
@@ -21,8 +21,30 @@ class Blog(db.Model):
 
 @app.route("/", methods = ['post'])
 def validatePost():
-    blogTitle = request.form["postTitle"]
-    blogText = request.form["newBlog"]
+    postTitle = request.form["postTitle"]
+    newBlog = request.form["newBlog"]
+    errorCount = 0
+    errorTitle = ""
+    errorPost = ""
+
+    if postTitle != "" and newBlog != "":
+        newTitleEscaped = cgi.escape(blogTitle, quote = True)
+        newTitle = Blog(newTitleEscaped)
+
+        newBlogEscaped = cgi.escape(blogText, quote = True)
+        newPost = Blog(newBlogEscaped)
+
+        db.session.add(newTitle)
+        db.session.add(newBlog)
+        db.session.commit()
+
+    else:
+        if blogTitle = "":
+            errorTitle = "Please fill in the title."
+            errorCount = errorCount + 1
+        if blogText = "":
+            errorPost = "Please fill in the body."
+            errorCount = errorCount + 1
 
 @app.route("/")
 def index():
