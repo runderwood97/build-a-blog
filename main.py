e41967809ff194810d97866cc6f1cec3afe04c1f
runderwood <runderwood2016@gmail.com>
2018-10-31 19:00:20 -0500

    added comments and functions to gather all records from Blog

--- main.py
+++ main.py
@@ -13,11 +13,19 @@ db = SQLAlchemy(app)
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(40))
-    blog = db.Column(db.String(100))
+    text = db.Column(db.String(100))
 
-    def __init__(self, title, blog):
+    def __init__(self, title, text):
         self.title = title
-        self.blog = blog
+        self.text = text
+
+def getBlogTitles():
+    # gathers all blog titles
+    return [title for title in Blog.query.all()]
+
+def getBlogText():
+    # gathers all blog texts
+    return [text for text in Blog.query.all()]
 
 @app.route("/", methods = ['post'])
 def validatePost():
@@ -27,6 +35,9 @@ def validatePost():
     errorTitle = ""
     errorPost = ""
 
+    # check to see if the blog title or post are empty
+    # if not empty add to database
+    # if they are empty increment errorCount and create error discription
     if postTitle != "" and newBlog != "":
         newTitleEscaped = cgi.escape(postTitle, quote = True)
         newTitle = Blog(newTitleEscaped)
@@ -37,7 +48,6 @@ def validatePost():
         db.session.add(newTitle)
         db.session.add(newPost)
         db.session.commit()
-
     else:
         if blogTitle = "":
             errorTitle = "Please fill in the title."
@@ -46,13 +56,16 @@ def validatePost():
             errorPost = "Please fill in the body."
             errorCount = errorCount + 1
 
+    # if an error was created return user to newPost.html with errors listed
+    # if no errors created send user user to blog.html with all posts
     if errorCount > 0:
         return render_template('newPost.html', titleError = errorTitle, blogError = errorPost)
     else:
-        return render_template('blog.html', )
+        return render_template('blog.html', title = newTitle, text = newPost)
 
 @app.route("/")
 def index():
+    # load up newPost.html 
     return render_template("newPost.html")
 
 app.run()
